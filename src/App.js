60e7c2c180ec194d758ce52279fbb01ca653464e
Jarol <jaancastano@utp.edu.co>
2025-10-18 16:59:15 -0500

    Apesar de muchos intentos no quiso funcionar la libreria de tailwind en mi proyecto react no se porque

--- src/App.js
+++ src/App.js
@@ -4,8 +4,13 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-        <div className='bg-red-500 text-blue-300'>
-          Hola mundo tailwind
+        <div className='flex flex-col md:flex-row'>
+          <div className='mx-2 bg-green-500'>elemento 1</div>
+          <div className='mx-2 bg-reed-800'>elemento 2</div>
+          <div className='mx-2 bg-prple-400'>elemento 3</div>
+          <div className='mx-2 bg-yellow-700'>elemento 4</div>
+          <button className='boton'>Haz clic aqui</button>
+          <button className='boton'>Boton 2</button>
         </div>     
     </div>
   );
